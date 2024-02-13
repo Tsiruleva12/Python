2f9d792d27121c21c324231e20582c687759cdc9
Tsiruleva12 <tsiruliovaelena@gmail.com>
2024-02-13 20:25:57 +0300

    Тестирую питон.

---
+++ Practice1/Main.py
@@ -0,0 +1,10 @@
+while True:
+    print("Выберите режим:\n1) Коммунистический;\n2) Демократический;\n3)Выход\n\nВвод: ", end="")
+    mode= int(input())
+
+    if mode == 1:
+        print("COmmunism!\n")
+    elif mode == 2:
+        print("Democracy!\n")
+    elif mode == 3:
+        break
